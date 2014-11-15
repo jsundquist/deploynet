0ceb5897214ed885558e54b1ef95cf404d697cac
jsundquist <jsundquist@gmail.com>
2014-11-14 20:43:35 -0600

    Adding in the angular seed project to convert the flat files to angular routing

--- app/app.js
+++ app/app.js
@@ -2,14 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-    'ngRoute',
-    //'deployNet.index'
-    'myApp.view1',
-    'myApp.view2',
-    'myApp.main',
-    'myApp.customerList',
-    'myApp.version'
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
 ]).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
