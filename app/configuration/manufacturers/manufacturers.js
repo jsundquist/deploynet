c4243b8b015439307b13b356bf8d0f71280fc9db
jsundquist <jsundquist@gmail.com>
2014-11-30 18:34:53 -0600

    Starting to add the functionality into the manufacturers section

--- app/configuration/manufacturers/manufacturers.js
+++ app/configuration/manufacturers/manufacturers.js
@@ -9,13 +9,42 @@ angular.module('myApp.manufacturers', ['ngRoute'])
         });
         $routeProvider.when('/configuration/manufacturer/:id?', {
             templateUrl: 'configuration/manufacturers/manufacturer_form.html',
-            controller: 'ManufacturersCtrl'
+            controller: 'ManufacturerCtrl'
         });
     }])
 
-    .controller('ManufacturersCtrl', [function() {
-
+    .controller('ManufacturersCtrl', ['$scope',function($scope) {
+        $scope.manufacturers = [{
+            name: 'test1',
+            id: 1
+        },{
+            name: 'test2',
+            id: 2
+        },{
+            name: 'test4',
+            id: 3
+        },{
+            name: 'test5',
+            id: 4
+        },{
+            name: 'test3',
+            id: 5
+        }];
     }])
-    .controller('ManufacturerCtrl', [function() {
-
+    .controller('ManufacturerCtrl', ['$scope','$routeParams', '$location',function($scope,$routeParams,$location) {
+        $scope.manufacturer = {
+            name: '',
+            id: 0
+        };
+        if ($routeParams.id) {
+            $scope.manufacturer = {
+                name: 'Some test manufacturer',
+                id: $routeParams.id
+            };
+        }
+        $scope.submit = function() {
+            if($scope.manufacturer.name) {
+                $location.path('/configuration/manufacturers')
+            }
+        }
     }]);
