fa56b2075b187da1a20303ff3e4f81389db80f74
Jonathan Sundquist <jsundquist@gmail.com>
2015-07-04 12:00:12 -0500

    Starting to move the customer form outside of the controller so it's easier to maintain.

---
+++ src/DeployNetBundle/Form/Type/CustomerType.php
@@ -0,0 +1,43 @@
+<?php
+namespace DeployNetBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class CustomerType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("name", "text")
+            ->add('address1', 'text')
+            ->add('address2', 'text')
+            ->add('address3', 'text')
+            ->add('city', 'text')
+            ->add('state', 'entity',
+                [
+                    'class' => 'DeployNetBundle:State',
+                    'property' => 'name'
+                ]
+            )
+            ->add('postalCode', 'text')
+            ->add('phoneNumber', 'text')
+            ->add('faxNumber', 'text')
+            ->add('save', 'submit', array('label' => 'Save'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DeployNetBundle\Entity\Customer',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'deploy_net_customer';
+    }
+}
